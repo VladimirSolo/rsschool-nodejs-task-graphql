1b9b79844960a5991520c308035df21cd59d4a93
Vladimir Solo <freeman.wushu@mail.ru>
2025-11-22 00:19:34 +0300

    feat: add validate graphql

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { schema, createLoaders } from './user.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -22,6 +23,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         prisma,
         loaders,
       };
+
+      const validationRules = [depthLimit(5)];
+      const documentAST = parse(query);
+
+      const errors = validate(schema, documentAST, validationRules);
+      if (errors.length > 0) {
+        return {
+          errors: errors.map((error) => ({
+            message: error.message,
+            locations: error.locations,
+            path: error.path,
+          })),
+        };
+      }
 
       try {
         const result = await graphql({
